69958d2600e0661854e2e19dbcffb25cb8737c8b
Thiago Buarque <thiago.buarqque@gmail.com>
2022-06-01 22:22:19 -0300

    Acho que esta pronto

--- MultilayerPerceptron/LossFunctions.py
+++ MultilayerPerceptron/LossFunctions.py
@@ -1,5 +1,9 @@
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_error
+)
 
 LOSS_FUNCTIONS = {
-    'mse': mean_squared_error
+    'mse': mean_squared_error,
+    'mae': mean_absolute_error
 }
